75a47c76088f2cc53320748a490d1258d540880c
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-04-05 21:14:24 -0500

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-import { useEthers, useContractFunction } from '@usedapp/core';
+import { useEthers, useContractFunction, TransactionState } from '@usedapp/core';
 import { utils, Contract } from 'ethers';
-import MintableERC20 from '../artifacts/contracts/MintableERC20.sol/MintableERC20.json';
+import MintableERC20 from './MintableERC20.json';
+import { Button, Grid, TextField, CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
 
-const contractAddress = "0xYourContractAddress"; // Replace with your contract address
+const contractAddress = "0x0394c0EdFcCA370B20622721985B577850B0eb75"; // Replace with your contract address
 
 function App() {
-  const { activateBrowserWallet, account } = useEthers();
+  const { activateBrowserWallet, deactivate, account } = useEthers();
   const [value, setValue] = useState(0);
   const contract = new Contract(contractAddress, MintableERC20.abi);
   const { state, send } = useContractFunction(contract, 'purchaseMint', { value });
@@ -15,22 +17,33 @@ function App() {
     send({ value: utils.parseEther(value.toString()) });
   };
 
+  const handleWalletConnection = () => {
+    if (account) deactivate();
+    else activateBrowserWallet();
+  };
+
   return (
-    <div>
-      <button onClick={() => activateBrowserWallet()}>
-        Connect Wallet
-      </button>
-      <div>
-        <input
+    <Grid container direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+      <Box position="absolute" top={8} right={16}>
+        <Button variant="contained" onClick={handleWalletConnection}>
+          {account ? `Disconnect ${account.substring(0, 5) + '...'}` : 'Connect Wallet'}
+        </Button>
+      </Box>
+      <Grid item xs={12}>
+        <TextField
           type="number"
           onChange={(e) => setValue(e.target.value)}
-          placeholder="Enter value in ETH"
+          label="Enter value in ETH"
+          variant="outlined"
+          style={{ marginBottom: '16px' }}
         />
-        <button onClick={handlePurchaseMint}>
-          Purchase Mint
-        </button>
-      </div>
-    </div>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={handlePurchaseMint} disabled={state.status == 'Mining' || account == null}>
+          {state.status == 'Mining' ? <CircularProgress size={24} style={{ marginLeft: '8px' }} /> : 'Purchase Mint'}
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
