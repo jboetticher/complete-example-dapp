c7bdaf711a661aea06b7c5a5d2c766db5a3d868e
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-04-10 13:49:04 -0500

    Formatting

--- frontend/src/MintingComponent.js
+++ frontend/src/MintingComponent.js
@@ -4,38 +4,37 @@ import { Button, CircularProgress, TextField, Grid } from "@mui/material";
 import { utils } from "ethers";
 
 export default function MintingComponent({ contract }) {
-    const [value, setValue] = useState(0);
-    const { account } = useEthers();
-    const { state, send } = useContractFunction(contract, 'purchaseMint', { value });
-  
-    // Mint transaction
-    const handlePurchaseMint = () => send({ value: utils.parseEther(value.toString()) });
-  
-    return (
-      <>
-        <Grid item xs={12}>
-          <TextField
-            type="number"
-            onChange={(e) => setValue(e.target.value)}
-            label="Enter value in DEV"
-            variant="outlined"
-            fullWidth
-            style={{ marginBottom: '16px' }} />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="contained" color="primary" fullWidth
-            onClick={handlePurchaseMint}
-            disabled={state.status === 'Mining' || account == null}
-          >
-            {state.status === 'Mining' ? (
-              <CircularProgress size={24} style={{ marginLeft: '8px' }} />
-            ) : (
-              'Purchase Mint'
-            )}
-          </Button>
-        </Grid>
-      </>
-    );
-  }
-  
+  const [value, setValue] = useState(0);
+  const { account } = useEthers();
+  const { state, send } = useContractFunction(contract, 'purchaseMint', { value });
+
+  // Mint transaction
+  const handlePurchaseMint = () => send({ value: utils.parseEther(value.toString()) });
+
+  return (
+    <>
+      <Grid item xs={12}>
+        <TextField
+          type="number"
+          onChange={(e) => setValue(e.target.value)}
+          label="Enter value in DEV"
+          variant="outlined"
+          fullWidth
+          style={{ marginBottom: '16px' }} />
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant="contained" color="primary" fullWidth
+          onClick={handlePurchaseMint}
+          disabled={state.status === 'Mining' || account == null}
+        >
+          {state.status === 'Mining' ? (
+            <CircularProgress size={24} style={{ marginLeft: '8px' }} />
+          ) : (
+            'Purchase Mint'
+          )}
+        </Button>
+      </Grid>
+    </>
+  );
+}
